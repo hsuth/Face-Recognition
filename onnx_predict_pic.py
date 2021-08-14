59ef509971226f482317cb29892f46bb16602835
hsuth <hsuth@mail.stust.edu.tw>
2021-08-15 01:17:17 +0800

    Update onnx_predict_pic.py

--- onnx_predict_pic.py
+++ onnx_predict_pic.py
@@ -8,6 +8,8 @@ from config import get_config
 from mtcnn import MTCNN
 from ONNXLearner import face_learner
 from utils import load_facebank, draw_box_name, prepare_facebank
+import sys
+import traceback
 
 
 parser = argparse.ArgumentParser(description='for face verification')
@@ -61,6 +63,16 @@ try:
           frame = draw_box_name(bbox, names[results[idx] + 1], frame)
   print('output '+ args.output)
   cv2.imwrite(args.output,frame)
-except:
-  print('error')
+
+except Exception as e:
+  error_class = e.__class__.__name__ #取得錯誤類型
+  detail = e.args[0] #取得詳細內容
+  cl, exc, tb = sys.exc_info() #取得Call Stack
+  lastCallStack = traceback.extract_tb(tb)[-1] #取得Call Stack的最後一筆資料
+  fileName = lastCallStack[0] #取得發生的檔案名稱
+  lineNum = lastCallStack[1] #取得發生的行號
+  funcName = lastCallStack[2] #取得發生的函數名稱
+  errMsg = "File \"{}\", line {}, in {}: [{}] {}".format(fileName, lineNum, funcName, error_class, detail)
+  print(errMsg)
   pass    
+
