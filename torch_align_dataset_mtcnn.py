34f11b0a13efa9d4f8e4e181dd179997cffaa2f2
hsuth <hsuth@mail.stust.edu.tw>
2021-08-15 15:05:13 +0800

    Create torch_align_dataset_mtcnn.py

---
+++ torch_align_dataset_mtcnn.py
@@ -0,0 +1,186 @@
+"""Performs face alignment and stores face thumbnails in the output directory."""
+# MIT License
+# 
+# Copyright (c) 2016 David Sandberg
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from scipy import misc
+import sys
+import os
+import argparse
+import numpy as np
+import random
+import imageio
+
+import cv2
+from PIL import Image
+import argparse
+from pathlib import Path
+from multiprocessing import Process, Pipe,Value,Array
+import torch
+from config import get_config
+from mtcnn import MTCNN
+from utils import load_facebank, draw_box_name, prepare_facebank
+
+from time import sleep
+
+class ImageClass():
+    "Stores the paths to images for a given class"
+    def __init__(self, name, image_paths):
+        self.name = name
+        self.image_paths = image_paths
+  
+    def __str__(self):
+        return self.name + ', ' + str(len(self.image_paths)) + ' images'
+  
+    def __len__(self):
+        return len(self.image_paths)
+
+def get_dataset(path, has_class_directories=True):
+    dataset = []
+    path_exp = os.path.expanduser(path)
+    classes = [path for path in os.listdir(path_exp) \
+                    if os.path.isdir(os.path.join(path_exp, path))]
+    classes.sort()
+    nrof_classes = len(classes)
+    for i in range(nrof_classes):
+        class_name = classes[i]
+        facedir = os.path.join(path_exp, class_name)
+        image_paths = get_image_paths(facedir)
+        dataset.append(ImageClass(class_name, image_paths))
+  
+    return dataset
+    
+#numpy to rgb?    
+def to_rgb(img):
+    w, h = img.shape
+    ret = np.empty((w, h, 3), dtype=np.uint8)
+    ret[:, :, 0] = ret[:, :, 1] = ret[:, :, 2] = img
+    return ret
+
+def main(args):
+    sleep(random.random())
+    output_dir = os.path.expanduser(args.output_dir)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    # Store some git revision info in a text file in the log directory
+    src_path,_ = os.path.split(os.path.realpath(__file__))
+    dataset = get_dataset(args.input_dir)
+    
+    conf = get_config(False)
+
+    mtcnn = MTCNN()
+    print('arcface loaded')
+
+    print('Creating networks and loading parameters')
+    
+  
+    # Add a random key to the filename to allow alignment using multiple processes
+    random_key = np.random.randint(0, high=99999)
+    bounding_boxes_filename = os.path.join(output_dir, 'bounding_boxes_%05d.txt' % random_key)
+    
+    with open(bounding_boxes_filename, "w") as text_file:
+        nrof_images_total = 0
+        nrof_successfully_aligned = 0
+        if args.random_order:
+            random.shuffle(dataset)
+        for cls in dataset:
+            output_class_dir = os.path.join(output_dir, cls.name)
+            if not os.path.exists(output_class_dir):
+                os.makedirs(output_class_dir)
+                if args.random_order:
+                    random.shuffle(cls.image_paths)
+            for image_path in cls.image_paths:
+                nrof_images_total += 1
+                filename = os.path.splitext(os.path.split(image_path)[1])[0]
+                output_filename = os.path.join(output_class_dir, filename+'.png')
+                print(image_path)
+                if not os.path.exists(output_filename):
+                  try:
+                    frame = cv2.imread(filename)
+                    image = Image.fromarray(frame[...,::-1]) #bgr to rgb
+
+                    #bounding_boxes, _ = align.detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+                    bboxes, faces = mtcnn.align_multi(image, conf.face_limit, conf.min_face_size)
+                    bboxes = bboxes[:,:-1] #shape:[10,4],only keep 10 highest possibiity faces
+                    bboxes = bboxes.astype(int)
+                    bboxes = bboxes + [-1,-1,1,1] # personal choice    
+                    
+                    # print(score[0])
+                    for idx,bbox in enumerate(bboxes):
+                      X= bbox[0]
+                      Y=bbox[1]
+                      W= bbox[2] - X
+                      H= bbox[3] -Y
+                      cropped = frame[X:W, Y:H]
+
+                      image = cv2.resize(image, (256, 256), interpolation=cv2.INTER_AREA)
+                      print('output '+ args.output)
+                      cv2.imwrite(args.output,frame)
+                      
+                    """
+                    nrof_faces = bounding_boxes.shape[0]
+                    if nrof_faces>0:
+                        det = bounding_boxes[:,0:4]
+                        det_arr = []
+                        img_size = np.asarray(img.shape)[0:2]
+                        if nrof_faces>1:
+                      
+                            cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
+                            scaled = misc.imresize(cropped, (args.image_size, args.image_size), interp='bilinear')
+                            nrof_successfully_aligned += 1
+                            filename_base, file_extension = os.path.splitext(output_filename)
+                            if args.detect_multiple_faces:
+                                output_filename_n = "{}_{}{}".format(filename_base, i, file_extension)
+                            else:
+                                output_filename_n = "{}{}".format(filename_base, file_extension)
+                            misc.imsave(output_filename_n, scaled)
+                            text_file.write('%s %d %d %d %d\n' % (output_filename_n, bb[0], bb[1], bb[2], bb[3]))
+                    else:
+                        print('Unable to align "%s"' % image_path)
+                        text_file.write('%s\n' % (output_filename))
+                    """        
+    print('Total number of images: %d' % nrof_images_total)
+    print('Number of successfully aligned images: %d' % nrof_successfully_aligned)
+            
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('input_dir', type=str, help='Directory with unaligned images.')
+    parser.add_argument('output_dir', type=str, help='Directory with aligned face thumbnails.')
+    parser.add_argument('--image_size', type=int,
+        help='Image size (height, width) in pixels.', default=182)
+    parser.add_argument('--margin', type=int,
+        help='Margin for the crop around the bounding box (height, width) in pixels.', default=44)
+    parser.add_argument('--random_order', 
+        help='Shuffles the order of images to enable alignment using multiple processes.', action='store_true')
+    parser.add_argument('--gpu_memory_fraction', type=float,
+        help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
+    parser.add_argument('--detect_multiple_faces', type=bool,
+                        help='Detect and align multiple faces per image.', default=False)
+    return parser.parse_args(argv)
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
